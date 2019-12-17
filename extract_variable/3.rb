0e209969afc4d3cc2d5a4f7b33c51a760bd6a0d6
Student <student@Makerss-MacBook-Pro-5.local>
2019-12-17 11:39:26 +0000

    further extract variable excercises

--- extract_variable/3.rb
+++ extract_variable/3.rb
@@ -5,3 +5,13 @@ def send_correct_page(browser, window)
     return "You are not using the Safari browser or have a big window."
   end
 end
+
+def send_correct_page(browser, window)
+  user_browser_type = browser.type
+  user_window_size = window.size
+  if(user_browser_type == "Safari" && user_window_size < "768px")
+    return "You are using the Safari browser in a small window."
+  else
+    return "You are not using the Safari browser or have a big window."
+  end
+end
