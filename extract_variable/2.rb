0c81488097f1364c991a3663a535b35f16f642bf
Student <student@Makerss-MacBook-Pro-5.local>
2019-12-17 11:10:06 +0000

    extract variable practice with if statement

--- extract_variable/2.rb
+++ extract_variable/2.rb
@@ -5,3 +5,13 @@ def tell_browser_type(browser)
     puts "You are using a non-Safari browser."
   end
 end
+
+def tell_browser_type(browser)
+  look_at_browser = browser.type
+  if browser.type == "Safari")
+    puts "You are using the Safari browser."
+  else
+    non_safari_message = "You are using a non-Safari browser"
+  end
+
+end
