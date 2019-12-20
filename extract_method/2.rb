b64a4340578c11751d5419ff9dec39513be06c3b
Student <katierodgers95@gmail.com>
2019-12-20 10:45:58 +0000

    browser type

--- extract_method/2.rb
+++ extract_method/2.rb
@@ -5,3 +5,18 @@ def tell_browser_type(browser)
 
   browser_type_is_safari ? puts safari_message : puts not_safari_message
 end
+
+class tell_browser_Type
+
+  def is_safari
+    browser_type_is_safari = browser.type == "Safari"
+    safari_message = "You are using the Safari browser."
+    browser_type_is_safari ? puts safari_message 
+  end
+
+  def is_not_safari
+    not_safari_message = "You are using a non-Safari browser."
+    puts not_safari_message
+  end
+
+end
